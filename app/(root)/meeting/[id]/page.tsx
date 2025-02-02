a5360b4925f0e6f7029f050ff2d1d2f43c7e192c
headlessripper <167107185+headlessripper@users.noreply.github.com>
2025-02-03 00:09:56 +0200

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,37 +1,35 @@
-'use client';
-
+'use client'
 import Loader from '@/components/ui/Loader';
 import MeetingRoom from '@/components/ui/MeetingRoom';
 import MeetingSetup from '@/components/ui/MeetingSetup';
 import { useGetCallByid } from '@/hooks/useGetCallByid';
-import { useUser } from '@clerk/nextjs';
+import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 
-interface PageProps {
-  params: { id: string };
-}
+const Meeting = ({ params: { id } }: {params: { id: string } }) => {
+  const { user, isLoaded} = useUser();
+  const [isSetupComplete, setisSetupComplete] = useState(false)
+  const {call, isCallLoading } = useGetCallByid(id);
 
-const Meeting = ({ params }: PageProps) => {
-  const { isLoaded } = useUser(); // Removed 'user' since it's unused
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
-  const { call, isCallLoading } = useGetCallByid(params.id);
-
-  if (!isLoaded || isCallLoading) return <Loader />;
+  if(!isLoaded || isCallLoading) return <Loader />
 
   return (
     <main className='h-screen w-full'>
       <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
-            <MeetingSetup setisSetupComplete={setIsSetupComplete} />
-          ) : (
+            <MeetingSetup setisSetupComplete={
+              setisSetupComplete
+            }/>
+          ): (
             <MeetingRoom />
           )}
         </StreamTheme>
       </StreamCall>
-    </main>
-  );
-};
 
-export default Meeting;
+    </main>
+  )
+}
+
+export default Meeting
