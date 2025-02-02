212d887fa04f43f77aa85de7bd2af3f003871f97
headlessripper <167107185+headlessripper@users.noreply.github.com>
2025-02-02 23:43:41 +0200

    Update MeetingModal.tsx

--- components/ui/MeetingModal.tsx
+++ components/ui/MeetingModal.tsx
@@ -1,64 +1,71 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
 import {
     Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
+    DialogContent
+} from "@/components/ui/dialog";
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 import { Button } from './button';
-  
 
-interface MeetingModalProps{
+interface MeetingModalProps {
     isOpen: boolean;
     onClose: () => void;
     title: string;
     className?: string;
     children?: ReactNode;
-    handleClick?:  () => void;
+    handleClick?: () => void;
     buttonText?: string;
     image?: string;
     buttonIcon?: string;
 }
-const MeetingModal = ({ isOpen, onClose, title, className, children, handleClick,
-     buttonText, image, buttonIcon}: MeetingModalProps ) => {
+
+const MeetingModal = ({ 
+    isOpen, 
+    onClose, 
+    title, 
+    className, 
+    children, 
+    handleClick, 
+    buttonText, 
+    image, 
+    buttonIcon
+}: MeetingModalProps ) => {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}> 
-        <DialogContent className='flex w-full max-w-[530px] flex-col
-        gap-6 border-none bg-dark-1 px-6 py-9
-        text-white'>
-            <div className=' flex justify-center'>
+        <DialogContent className="flex w-full max-w-[530px] flex-col gap-6 border-none bg-dark-1 px-6 py-9 text-white">
+            <div className="flex flex-col items-center gap-4">
                 {image && (
-                    <div className='flex flex-col gap-2'>
-                        <Image 
+                    <Image 
                         src={image}
-                        alt='image'
+                        alt="image"
                         width={72}
-                        height={72}/>
-                    </div>
+                        height={72}
+                    />
                 )}
-                <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>{title}</h1>
+                <h1 className={cn('text-3xl font-bold leading-[42px]', className)}>
+                    {title}
+                </h1>
 
                 {children}
-                <Button className='bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0 gap-6' onClick={handleClick}>
+
+                <Button 
+                    className="bg-blue-1 focus-visible:ring-0 focus-visible:ring-offset-0 flex items-center gap-2"
+                    onClick={handleClick}
+                >
                     {buttonIcon && (
                         <Image 
-                        src={buttonIcon}
-                        alt='button icon'
-                        width={13}
-                        height={13}/>
-                    )}&nbsp;
+                            src={buttonIcon}
+                            alt="button icon"
+                            width={13}
+                            height={13}
+                        />
+                    )}
                     {buttonText || 'Schedule Class'}
                 </Button>
             </div>
-            
         </DialogContent>
     </Dialog>
+  );
+};
 
-  )
-}
-
-export default MeetingModal
+export default MeetingModal;
