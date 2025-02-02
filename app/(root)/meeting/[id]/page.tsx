83fcf2ed46611b305b83a4a5fe57f9dfb17af270
headlessripper <167107185+headlessripper@users.noreply.github.com>
2025-02-03 00:18:06 +0200

    Update page.tsx

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -8,28 +8,25 @@ import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 
 const Meeting = ({ params: { id } }: {params: { id: string } }) => {
-  const { user, isLoaded} = useUser();
-  const [isSetupComplete, setisSetupComplete] = useState(false)
-  const {call, isCallLoading } = useGetCallByid(id);
+  const { isLoaded } = useUser();  // Remove 'user' if not needed
+  const [isSetupComplete, setisSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallByid(id);
 
-  if(!isLoaded || isCallLoading) return <Loader />
+  if (!isLoaded || isCallLoading) return <Loader />
 
   return (
     <main className='h-screen w-full'>
       <StreamCall call={call}>
         <StreamTheme>
           {!isSetupComplete ? (
-            <MeetingSetup setisSetupComplete={
-              setisSetupComplete
-            }/>
-          ): (
+            <MeetingSetup setisSetupComplete={setisSetupComplete} />
+          ) : (
             <MeetingRoom />
           )}
         </StreamTheme>
       </StreamCall>
-
     </main>
   )
 }
 
-export default Meeting
+export default Meeting;
